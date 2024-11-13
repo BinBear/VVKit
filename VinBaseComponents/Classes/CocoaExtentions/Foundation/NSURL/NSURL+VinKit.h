90df215eca4fc2666a5d562b6f65e59411b54155
BinBear <vin404@outlook.com>
2024-11-13 14:58:36 +0800

    Update NSURL+VinKit.h

--- VinBaseComponents/Classes/CocoaExtentions/Foundation/NSURL/NSURL+VinKit.h
+++ VinBaseComponents/Classes/CocoaExtentions/Foundation/NSURL/NSURL+VinKit.h
@@ -15,6 +15,9 @@ NS_ASSUME_NONNULL_BEGIN
 /// 解析URL,将URL中的参数解析为字典
 - (nullable NSMutableDictionary *)vv_urlToParameter;
 
+/// 删除URL参数中某个参数
+- (NSString *)vv_removeParaKey:(NSString *)key;
+
 @end
 
 NS_ASSUME_NONNULL_END
